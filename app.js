db8a8a70bd068e027eca47882392ec843cf06736
Sujan Thapa <84758577+thapasujan@users.noreply.github.com>
2025-06-15 22:28:15 +0545

    add comment

--- app.js
+++ app.js
@@ -20,27 +20,64 @@ app.get("/", (req, res) => {
 
 // Create blog API
 app.post("/createBlog", async (req, res) => {
-//   console.log(req.body);
+  //   console.log(req.body);
 
-    const title = req.body.title;
-    const subTitle = req.body.subTitle;
-    const description = req.body.description;
+  const title = req.body.title;
+  const subTitle = req.body.subTitle;
+  const description = req.body.description;
 
-    // Alternative (object destructuring)
-    // const {title, subTitle, description} = req.body:
-
+  // Alternative (object destructuring)
+  // const {title, subTitle, description} = req.body:
 
   // Insert into database
   await Blog.create({
     title: title,
     subTitle: subTitle,
     description: description,
-  })
+  });
 
   res.json({
     status: 200,
     message: "Success",
   });
+});
+
+app.get("/blogs", async (req, res) => {
+  // fetching data from database
+  const blogs = await Blog.find();
+
+  // check if blogs contains data or not
+  if (blogs.length === 0) {
+    res.json({
+      status: 404,
+      message: "No data found",
+    });
+  } else {
+    res.json({
+      status: 200,
+      message: "Success",
+      data: blogs,
+    });
+  }
+});
+
+// GET API - /blogs/:id (single API)
+app.get("/blogs/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  // const {id} = req.params // alternative
+  const blog_data = await Blog.find({ _id : id })
+  // const blog_data = await Blog.findById(id);
+  if (blog_data.length == 0) {
+    res.status(404).json({
+      message: "No data found with this id"
+    });
+  } else {
+    res.status(200).json({
+      message: "got data",
+      data: blog_data,
+    });
+  }
 });
 
 app.listen(3000, (req, res) => {
